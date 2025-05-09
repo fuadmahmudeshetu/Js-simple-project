2d4c35b86a3c065fdeae2cbc9265feaaab932353
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-05-10 00:10:38 +0300

    fill prompt input with random example

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const themeToggle = document.querySelector(".theme-toggle");
+const promptBtn = document.querySelector(".prompt-btn");
+const promptInput = document.querySelector(".prompt-input");
+
+
+const examplePrompts = [
+    "A magic forest with glowing plants and fairy homes among giant mushrooms",
+    "An old steampunk airship floating through golden clouds at sunset",
+    "A future mars colony with glass domes and gardens against red mountains",
+    "A dragon sleeping on gold coins in a crystal cave",
+    "An underwater kingdom with more people and growing coral buildings",
+    "A floating island with waterfalls pouring into clouds below"
+
+]
 
 // Set theme based on saved preference or system default
 (()=> {
@@ -15,6 +28,13 @@ const toggleTheme = () => {
     const isDarkTheme = document.body.classList.toggle("dark-theme");
     localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
     themeToggle.querySelector("i").className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
-}
+};
+
+// fill prompt input with random example
+promptBtn.addEventListener("click", ()=>{
+    const prompt = examplePrompts[Math.floor(Math.random()*examplePrompts.length)];
+    promptInput.value  = prompt;
+    promptInput.focus();
+});
 
 themeToggle.addEventListener('click', toggleTheme);
